d11c8273db20841615599a25f614bd66d6ea2d3f
Akira Kanno <teinen.github@gmail.com>
2022-11-06 17:33:35 +0900

    Generate from templates

--- lib/generators/initializer_generator.rb
+++
@@ -1,8 +0,0 @@
-class InitializerGenerator < Rails::Generators::Base
-  desc "This generator creates an initializer file at config/initializers"
-
-  def create_initializer_files
-    create_file "config/initializers/initialier.rb",
-                "# Add initialization content here"
-  end
-end
