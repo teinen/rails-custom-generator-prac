e5e4c7a19dd2fef8113b4c3de1475c3d188f7f16
Akira Kanno <teinen.github@gmail.com>
2022-11-06 18:15:25 +0900

    Add helper generator

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module RailsCustomGeneratorPrac
       g.orm             :active_record
       g.template_engine :erb
       g.test_framework  :test_unit, fixture: false
+      g.stylesheets     false
+      g.helper          :my_helper
     end
   end
 end
